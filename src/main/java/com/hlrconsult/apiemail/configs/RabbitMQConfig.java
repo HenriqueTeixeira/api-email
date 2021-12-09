e5d8f5e1f448055338bc0e840c42e83f5427e58b
HenriqueTeixeira <hteixeira2007@gmail.com>
2021-12-09 10:11:25 -0300

    Correção das configurações do RabbitMQ.

--- src/main/java/com/hlrconsult/apiemail/configs/RabbitMQConfig.java
+++ src/main/java/com/hlrconsult/apiemail/configs/RabbitMQConfig.java
@@ -5,61 +5,60 @@ import java.util.Map;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.BindingBuilder.DirectExchangeRoutingKeyConfigurer;
-import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
-	private static Map<String, Object> args = new HashMap<String, Object>();
+	@Value("${spring.rabbitmq.mailqueue}")
+	private String mailQueue;
 
-	public final static String MAIL_QUEUE = "mail.queue";
+	@Value("${spring.rabbitmq.deadqueue}")
+	private String deadQueue;
 
 	private final static String MAIL_EXCHANGE = "mail.exchange";
 
-	private final static String DEAD_LETTER_QUEUE = "dead.letter.mail.queue";
-
 	private final static String DEAD_LETTER_EXCHANGE = "dead.letter.mail.exchange";
+
+	public static Map<String, Object> args = new HashMap<String, Object>();
 
 	static {
 		args.put("x-dead-letter-exchange", DEAD_LETTER_EXCHANGE);
-		// args.put("x-message-ttl", 5000);
-	}
-
-	// @Value("${spring.rabbitmq.mailqueue}")
-	// private String mailQueue;
-
-	@Bean
-	public Queue mailQueue() {
-		return new Queue(MAIL_QUEUE, true, false, false, args);
 	}
 
 	@Bean
-	public DirectExchange emailExchange() {
-		return new DirectExchange(MAIL_EXCHANGE, true, false);
+	public TopicExchange emailExchange() {
+		return new TopicExchange(MAIL_EXCHANGE, true, false);
 	}
 
 	@Bean
 	public Binding mailExchangeBinding(Queue mailQueue) {
-		return BindingBuilder.bind(mailQueue).to(emailExchange()).with(mailQueue.getName());
+		return BindingBuilder.bind(mailQueue).to(emailExchange()).with("*");
+	}
+
+	@Bean
+	public Queue mailQueue() {
+		return new Queue(mailQueue, true, false, false, args);
 	}
 
 	@Bean
 	public Queue deadLetterQueue() {
-		return new Queue(DEAD_LETTER_QUEUE, true, false, false);
+		return new Queue(deadQueue, true);
 	}
 
 	@Bean
-	public DirectExchange deadLetterExchange() {
-		return new DirectExchange(DEAD_LETTER_EXCHANGE);
+	public FanoutExchange deadLetterExchange() {
+		return new FanoutExchange(DEAD_LETTER_EXCHANGE);
 	}
 
 	@Bean
-	public DirectExchangeRoutingKeyConfigurer deadLetterBinding() {
+	public Binding deadLetterBinding() {
 		return BindingBuilder.bind(deadLetterQueue()).to(deadLetterExchange());
 	}
 
@@ -68,17 +67,4 @@ public class RabbitMQConfig {
 		return new Jackson2JsonMessageConverter();
 	}
 
-	/*
-	 * @Bean public RabbitListenerContainerFactory<DirectMessageListenerContainer>
-	 * rabbitListenerContainerFactory( ConnectionFactory connectionFactory) {
-	 * DirectRabbitListenerContainerFactory factory = new
-	 * DirectRabbitListenerContainerFactory();
-	 * 
-	 * factory.setConnectionFactory(connectionFactory);
-	 * factory.setAcknowledgeMode(AcknowledgeMode.AUTO);
-	 * factory.setPrefetchCount(4); //factory.setGlobalQos(true);
-	 * //factory.setConsumersPerQueue(20);
-	 * 
-	 * return factory; }
-	 */
 }
--- src/main/java/com/hlrconsult/apiemail/enums/StatusEmailEnum.java
+++ src/main/java/com/hlrconsult/apiemail/enums/StatusEmailEnum.java
@@ -1,5 +1,16 @@
 package com.hlrconsult.apiemail.enums;
 
 public enum StatusEmailEnum {
-	SENT,ERROR;
+
+	PENDING(0), PROCESSING(1), ERROR(2), SENT(3), NOT_SENT(4);
+
+	private int status;
+
+	StatusEmailEnum(int status) {
+		this.status = status;
+	}
+
+	public int getStatus() {
+		return status;
+	}
 }
